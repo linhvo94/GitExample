088d58ad930d0e29f0ac2634024b08af5dcf9f28
s3655204@rmit.edu.vn <s3655204@s00801001m-13.rmit.edu.vn>
2018-08-29 11:10:36 +0700

    "Commit"

--- GitExample/ViewController.swift
+++ GitExample/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print("Hello from the Xcode side")
     }
 
     override func didReceiveMemoryWarning() {
